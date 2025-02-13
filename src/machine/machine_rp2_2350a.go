3fbf6a77afab1881923a63ef72538c95f6e94e91
deadprogram <ron@hybridgroup.com>
2025-02-13 19:38:07 +0100

    machine: separate definitions for ADC pins on rp2350/rp2350b
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ src/machine/machine_rp2_2350a.go
@@ -0,0 +1,13 @@
+//go:build rp2350 && !rp2350b
+
+package machine
+
+// Analog pins on RP2350.
+const (
+	ADC0 Pin = GPIO26
+	ADC1 Pin = GPIO27
+	ADC2 Pin = GPIO28
+	ADC3 Pin = GPIO29
+
+	thermADC = 30
+)
