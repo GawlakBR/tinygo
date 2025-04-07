1b47f1e4e23e474f9b216d9fb6ddbd17bfe5919f
Ella Fox <ella@fox.gal>
2025-04-07 10:29:02 +0200

    Add I2C defaults

--- src/machine/board_btt_skr_pico.go
+++ src/machine/board_btt_skr_pico.go
@@ -11,71 +11,90 @@ package machine
 // TMC stepper driver motor direction.
 // X/Y/Z/E refers to motors for X/Y/Z and the extruder.
 const (
-	X_DIR Pin = GPIO10
-	Y_DIR Pin = GPIO5
-	Z_DIR Pin = ADC2
-	E_DIR Pin = GPIO13
+	X_DIR = GPIO10
+	Y_DIR = GPIO5
+	Z_DIR = ADC2
+	E_DIR = GPIO13
 )
 
 // TMC stepper driver motor step
 const (
-	X_STEP Pin = GPIO11
-	Y_STEP Pin = GPIO6
-	Z_STEP Pin = GPIO19
-	E_STEP Pin = GPIO14
+	X_STEP = GPIO11
+	Y_STEP = GPIO6
+	Z_STEP = GPIO19
+	E_STEP = GPIO14
 )
 
 // TMC stepper driver enable
 const (
-	X_ENABLE Pin = GPIO12
-	Y_ENABLE Pin = GPIO7
-	Z_ENABLE Pin = GPIO2
-	E_ENABLE Pin = GPIO15
+	X_ENABLE = GPIO12
+	Y_ENABLE = GPIO7
+	Z_ENABLE = GPIO2
+	E_ENABLE = GPIO15
 )
 
 // TMC stepper driver UART
 const (
-	TMC_UART_TX Pin = GPIO8
-	TMC_UART_RX Pin = GPIO9
+	TMC_UART_TX = GPIO8
+	TMC_UART_RX = GPIO9
 )
 
 // Endstops
 const (
-	X_ENDSTOP Pin = GPIO4
-	Y_ENDSTOP Pin = GPIO3
-	Z_ENDSTOP Pin = GPIO25
-	E_ENDSTOP Pin = GPIO16
+	X_ENDSTOP = GPIO4
+	Y_ENDSTOP = GPIO3
+	Z_ENDSTOP = GPIO25
+	E_ENDSTOP = GPIO16
 )
 
 // Fan PWM
 const (
-	FAN1_PWM Pin = GPIO17
-	FAN2_PWM Pin = GPIO18
-	FAN3_PWM Pin = GPIO20
+	FAN1_PWM = GPIO17
+	FAN2_PWM = GPIO18
+	FAN3_PWM = GPIO20
 )
 
 // Heater PWM
 const (
-	HEATER_BED_PWM      Pin = GPIO21
-	HEATER_EXTRUDER_PWM Pin = GPIO23
+	HEATER_BED_PWM      = GPIO21
+	HEATER_EXTRUDER_PWM = GPIO23
 )
 
 // Thermistors
 const (
-	THERM_BED          = ADC0 // Bed heater
-	THERM_EXTRUDER Pin = ADC1 // Toolhead heater
+	THERM_BED      = ADC0 // Bed heater
+	THERM_EXTRUDER = ADC1 // Toolhead heater
 )
 
 // Misc
 const (
-	RGB        Pin = GPIO24 // Neopixel
-	SERVO_ADC3 Pin = ADC3   // Servo
-	PROBE      Pin = GPIO22 // Probe
+	RGB        = GPIO24 // Neopixel
+	SERVO_ADC3 = ADC3   // Servo
+	PROBE      = GPIO22 // Probe
 )
 
 // Onboard crystal oscillator frequency, in MHz.
 const (
 	xoscFreq = 12 // MHz
+)
+
+// I2C. We don't have this available, so map it to defaults
+const (
+	I2C0_SDA_PIN = GPIO4
+	I2C0_SCL_PIN = GPIO5
+
+	I2C1_SDA_PIN = GPIO2
+	I2C1_SCL_PIN = GPIO3
+)
+
+// SPI default pins
+const (
+	SPI0_SCK_PIN = GPIO18
+	SPI0_SDO_PIN = GPIO19 // Tx
+	SPI0_SDI_PIN = GPIO16 // Rx
+	SPI1_SCK_PIN = GPIO10
+	SPI1_SDO_PIN = GPIO11 // Tx
+	SPI1_SDI_PIN = GPIO12 // Rx
 )
 
 // USB CDC identifiers
@@ -93,6 +112,8 @@ var (
 const (
 	UART0_TX_PIN = GPIO0
 	UART0_RX_PIN = GPIO1
+	UART1_TX_PIN = TMC_UART_TX
+	UART1_RX_PIN = TMC_UART_RX
 	UART_TX_PIN  = UART0_TX_PIN
 	UART_RX_PIN  = UART0_RX_PIN
 )
